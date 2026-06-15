60db5bba8a3e5c54061309d3cd86879155104932
Sumanth <sumanthveera631@gmail.com>
2026-06-15 20:04:38 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -13,6 +13,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${BACKEND_URL}/api/auth/google/callback`,
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
